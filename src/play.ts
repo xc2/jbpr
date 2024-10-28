7872d76bcb2a9cac1bd3a0a3bd6af7a9c5ce6c82
xc2 <gh@109cafe.com>
2024-10-28 20:25:36 +0800

    fix: invalid plugin descriptor

--- src/play.ts
+++ src/play.ts
@@ -1,29 +1,20 @@
+import { configure } from "@zip.js/zip.js";
+import { unstreamText } from "./utils/stream";
 import { getZipTransformStream } from "./utils/zip";
 
-import { rm } from "node:fs/promises";
+configure({ useCompressionStream: false });
 
-await rm("plugin.zip", { force: true });
-await rm("github-copilot-intellij", { recursive: true, force: true });
-const file = Bun.file(Bun.argv[2]);
+const file = Bun.file(Bun.argv[2]!);
 
 const target = file.stream().pipeThrough(
   getZipTransformStream((entry) => {
     if (entry.filename.endsWith(".jar")) {
       return getZipTransformStream((entry) => {
         if (entry.filename === "META-INF/plugin.xml") {
-          // console.log("input", entry);
-          // return unstreamText((text) => {
-          //   text = text.replace(/ since-build="[^"]+"/, "");
-          //   text = text.replace(/ until-build="[^"]+"/, "");
-          //   return text;
-          // });
-        } else if (entry.filename === "META-INF/MANIFEST.MF") {
-          //           return unstreamText((text) => {
-          //             // text = text.replace(/ since-build="[^"]+"/, "");
-          //             return `Manifest-Version: 1.0
-          // Created-By: 21.0.4 (Microsoft)
-          // `;
-          //           });
+          return unstreamText((text) => {
+            text = text.replace(/ until-build="[^"]+"/, "");
+            return text;
+          });
         }
       });
     }
