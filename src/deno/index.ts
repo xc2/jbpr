fbaefadd9a9ead0fc03d4cd2479f35f37353a0fb
Kaffi Y. <gh@109cafe.com>
2024-10-30 21:55:33 +0800

    feat: change the default hostname from localhost to 127.0.0.1; support unix socket binding (#3)

--- src/deno/index.ts
+++ src/deno/index.ts
@@ -1,22 +1,42 @@
-#!/usr/bin/env deno run --allow-net --unstable-sloppy-imports
+#!/usr/bin/env deno run --allow-net --unstable-sloppy-imports --allow-read --allow-write
+import { resolve as resolvePath } from "node:path";
 import { parseArgs } from "@std/cli/parse-args";
 import { Hono } from "hono";
 import { getUsageMessageHandler, registerHandlers } from "../mod";
 import { getUsageMessage, normalizeArgs } from "../utils/cli";
 
-const args = normalizeArgs(parseArgs(Deno.args), Deno.exit);
+const { binding } = normalizeArgs(parseArgs(Deno.args), Deno.exit);
 
 const app = new Hono<{ Bindings: {} }>();
 registerHandlers(app);
+app.get("/", getUsageMessageHandler());
 
 Deno.serve(
   {
-    hostname: args.hostname,
-    port: args.port,
-    onListen(addr) {
-      app.get("/", getUsageMessageHandler(addr));
-      console.log(getUsageMessage(addr));
+    onListen(addr: Deno.NetAddr | Deno.UnixAddr) {
+      console.log(getUsageMessage({ url: addrToUrl(addr) }));
     },
+    ...getBinding(binding),
   },
   app.fetch
 );
+
+function getBinding(b: typeof binding) {
+  if (b.unix) {
+    return { path: resolvePath(Deno.cwd(), b.unix) };
+  }
+  return b;
+}
+
+function addrToUrl(addr: Deno.NetAddr | Deno.UnixAddr) {
+  if (addr.transport === "unix" || addr.transport === "unixpacket") {
+    return new URL(`unix://${addr.path}`);
+  }
+  if (addr.transport === "tcp" || addr.transport === "udp") {
+    if (addr.hostname.includes(":")) {
+      return new URL(`http://[${addr.hostname}]:${addr.port}`);
+    }
+    return new URL(`http://${addr.hostname}:${addr.port}`);
+  }
+  throw new Error("unreachable");
+}
--- src/utils/cli.ts
+++ src/utils/cli.ts
@@ -3,41 +3,63 @@ import { RepositoryPath } from "../paths";
 export function normalizeArgs(_args: unknown, exit: (code: number) => any) {
   const args = _args as any;
   if (args.help === true || args.h === true) {
-    console.error(`OPTIONS:
+    console.error(`OPTIONS
 
---help, -h: Show this help message
---hostname <hostname>, -H <hostname>: Hostname to bind to
---port <port>, -p <port>: Port to bind to
+  --help, -h: Show this help message
+  --hostname <hostname>, -H <hostname>: Hostname to bind to
+  --port <port>, -p <port>: Port to bind to
+  --socket <socket>, -s <socket>: Unix socket to bind to
+
+DEFAULTS
+
+  --hostname 127.0.0.1 --port 8686
 `);
     exit(1);
     throw new Error("unreachable");
   }
 
-  const hostname = `${args.hostname || args.H || "localhost"}`;
-  const port = Number(args.port || args.p || 8686);
-  return { hostname, port };
+  const binding = getBinding({
+    hostname: args.hostname || args.H,
+    port: args.port || args.p,
+    socket: args.socket || args.s,
+  });
+
+  return { binding };
 }
 
-export function getUsageMessage({
+function getBinding({
   hostname,
   port,
-  origin,
-}: { hostname?: string; port?: number; origin?: string }) {
-  if (hostname && ["127.0.0.1", "127.1", "::1"].includes(hostname)) {
-    hostname = "localhost";
+  socket,
+}: { hostname?: string; port?: number | string; socket?: string }):
+  | { unix: string; hostname?: undefined; port?: undefined }
+  | { hostname: string; port: number; unix?: undefined } {
+  if (socket) {
+    return { unix: socket };
+  } else {
+    hostname = `${hostname || "127.0.0.1"}`;
+    port = Number(port || 8686);
+    return { hostname, port };
   }
-  origin = origin || `http://${hostname!.includes(":") ? `[${hostname}]` : hostname}:${port}`;
-  const u = new URL(RepositoryPath, origin);
+}
 
-  return `Service is listening on ${u.origin}
-  
-REPOSITORY URL:
+export function getUsageMessage({ url }: { url: URL }) {
+  const u = new URL(RepositoryPath, url);
 
-${u.href} (Replace :id with the plugin ID)
+  const repository = u.protocol === "unix:" ? "<origin>" + u.pathname : u.href;
 
-EXAMPLE:
+  return `Service is listening on
 
-${u.href.replace(/:id/, "17718")}
-  
-You can now add this repository to your IDE.`;
+  ${url.href}
+
+REPOSITORY URL
+
+  ${repository} (Replace :id with the plugin ID)
+
+EXAMPLE
+
+  ${repository.replace(/:id/, "17718")}
+
+You can now add this repository to your IDE.
+`;
 }
